194a1bbdc4693366a887ecb0753eec2c0ed4d557
CatFish <3285109052@qq.com>
2024-12-02 16:41:46 +0800

    连接远程仓库

--- chatgpt-data-app/src/main/java/cn/cat/chat/data/Application.java
+++ chatgpt-data-app/src/main/java/cn/cat/chat/data/Application.java
@@ -1,7 +1,5 @@
 package cn.cat.chat.data;
 
-
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 // 开启定时任务
 @EnableScheduling
 @Configurable
-@MapperScan("cn.cat.chat.data.infrastructure.dao")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
